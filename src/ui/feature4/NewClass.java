b76d3e205a5d2157955db326136ecb0c4e2c923f
aitor <aitor@2DAM-008>
2025-09-25 12:06:41 +0200

    My first commit on dev_feature4

---
+++ src/ui/feature4/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui.feature4;
+
+/**
+ *
+ * @author aitor
+ */
+public class NewClass {
+    
+}
