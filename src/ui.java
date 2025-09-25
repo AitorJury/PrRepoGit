3613bf100f477a3738c1c1fb17f0a6411dbbbfef
madrid <madrid@2DAM-002>
2025-09-25 12:19:00 +0200

    un commit

--- src/ui.java
+++ src/ui.java
@@ -3,5 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ui.feature5;
-    //comentario
+
+/**
+ *
+ * @author madrid
+ */
+public class ui {
+    
+}
